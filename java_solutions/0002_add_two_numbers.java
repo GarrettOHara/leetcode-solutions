97b585f333e36909b0de64d3143f1a4d94c2016a
Garrett O'Hara <garrett.ohara@nbcuni.com>
2025-04-22 09:18:58 -0700

    Adding another solution to 2 add two numbers, and adding ListNode class

--- java_solutions/0002_add_two_numbers.java
+++ java_solutions/0002_add_two_numbers.java
@@ -1,38 +1,37 @@
-/**
-You are given two non-empty linked lists representing two non-negative integers. 
-The digits are stored in reverse order, and each of their nodes contains a 
-single digit. Add the two numbers and return the sum as a linked list.
+/*
+ * You are given two non-empty linked lists representing two non-negative integers. 
+ * The digits are stored in reverse order, and each of their nodes contains a 
+ * single digit. Add the two numbers and return the sum as a linked list.
+ * 
+ * You may assume the two numbers do not contain any leading zero, except the 
+ * number 0 itself.
+ *  
+ * 
+ * Example 1:
+ * 
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ * 
+ * Example 2:
+ * 
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+ * 
+ * Example 3:
+ * 
+ * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * Output: [8,9,9,9,0,0,0,1]
+ * 
+ * 
+ * Constraints:
+ * 
+ *     The number of nodes in each linked list is in the range [1, 100].
+ *     0 <= Node.val <= 9
+ *     It is guaranteed that the list represents a number that does not have leading zeros.
+ */
 
-You may assume the two numbers do not contain any leading zero, except the 
-number 0 itself.
- 
-
-Example 1:
-
-Input: l1 = [2,4,3], l2 = [5,6,4]
-Output: [7,0,8]
-Explanation: 342 + 465 = 807.
-
-Example 2:
-
-Input: l1 = [0], l2 = [0]
-Output: [0]
-
-Example 3:
-
-Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
-Output: [8,9,9,9,0,0,0,1]
-
-
-Constraints:
-
-    The number of nodes in each linked list is in the range [1, 100].
-    0 <= Node.val <= 9
-    It is guaranteed that the list represents a number that does not have leading zeros.
-
-**/
-
-/**
+/*
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
@@ -65,31 +64,69 @@ class Solution {
     }
 }
 
-/**
-First Attempt:
+/*
+ * First Attempt:
+ * 
+ *     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+ *         ListNode res = l1;
+ *         int carry = 0;
+ *         
+ *         while(l1 != null && l2 != null) {
+ *             l1.val += l2.val + carry;
+ * 
+ *             if (l1.val > 9) {
+ *                 l1.val = l1.val % 10;
+ *                 carry = 1;
+ *             } else {
+ *                 carry = 0;
+ *             }
+ * 
+ *             l1 = l1.next;
+ *             l2 = l2.next;
+ *         }
+ * 
+ *         if (carry > 0) {
+ *             l1.next = new ListNode(1);
+ *         }
+ * 
+ *         return res;
+ *     }
+ */
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode res = l1;
-        int carry = 0;
-        
-        while(l1 != null && l2 != null) {
-            l1.val += l2.val + carry;
-
-            if (l1.val > 9) {
-                l1.val = l1.val % 10;
-                carry = 1;
-            } else {
-                carry = 0;
-            }
-
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-
-        if (carry > 0) {
-            l1.next = new ListNode(1);
-        }
-
-        return res;
-    }
+ /*
+ * Another attempt 04/22/25
+ * 
+ * class Solution {
+ *     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+ *         ListNode curr = new ListNode();
+ *         ListNode res = curr;
+ *         int carry = 0;
+ * 
+ *         while (l1 != null || l2 != null) {
+ *             int sum = 0;
+ *             if (l1 != null && l2 != null) {
+ *                 sum = l1.val + l2.val + carry;
+ *                 l1 = l1.next;
+ *                 l2 = l2.next;
+ *             } else if (l1 != null) {
+ *                 sum = l1.val + carry;
+ *                 l1 = l1.next;
+ *             } else if (l2 != null) {
+ *                 sum = l2.val + carry;
+ *                 l2 = l2.next;
+ *             }
+ *             if (sum >= 10)
+ *                 carry = 1;
+ *             else
+ *                 carry = 0;
+ *             curr.next = new ListNode(sum % 10);
+ *             curr = curr.next;
+ *         }
+ *         if (carry > 0) {
+ *             curr.next = new ListNode(carry);
+ *         }
+ *         
+ *         return res.next;
+ *     }
+ * }
  */
